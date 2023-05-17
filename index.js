7602964bde19521d18268544ad3db2ce0c64303e
root <root@DESKTOP-6GBLENF>
2023-05-17 22:13:29 +0300

    step2 complete

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+  //step - 1
+  const con = content.trim().split('\n').slice(1);
+  console.log(`Всего партий: ${con.length}`);
+  
+  
+  //step - 2
+  const rated = con.filter((el) => el.split(',')[1] === 'TRUE');
+  const unRate = con.filter((el) => el.split(',')[1] === 'FALSE');
+  const avRate = Math.round((rated.length / con.length) * 100);
+  const avUnRate = Math.round((unRate.length / con.length) * 100);
+  console.log(`Кол-во рейтинговых игр: ${rated.length}\nСоотношение:${avRate} : ${avUnRate}`);
   // END
 }
